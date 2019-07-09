a7927deba0a23a4023c77144091eb9409b91e7ae
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-07-09 14:52:10 -0700

    12 - scopes

--- src/12_scopes.py
+++ src/12_scopes.py
@@ -6,7 +6,10 @@ x = 12
 
 
 def changeX():
+    global x
     x = 99
+
+# https://www.programiz.com/python-programming/global-keyword
 
 
 changeX()
@@ -14,16 +17,19 @@ changeX()
 # This prints 12. What do we have to modify in changeX() to get it to print 99?
 print(x)
 
-
 # This nested function has a similar problem.
+
 
 def outer():
     y = 120
 
     def inner():
+        nonlocal y
         y = 999
 
     inner()
+
+    # https://www.programiz.com/python-programming/global-local-nonlocal-variables
 
     # This prints 120. What do we have to change in inner() to get it to print
     # 999? Google "python nested function scope".
